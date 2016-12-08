6c63f6cb7230a98b37a4e4bafb1c505d420daa9a
Rocky <rocky.amdad@gmail.com>
2016-12-08 17:09:37 +0600

    angular 2 heroes docuemntaion implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+  <h1>Welcome to {{title}}</h1>
+  <h2>Detail {{name}}</h2>
+  <div><label> id:</label>  {{id}}</div>
+    <div><label>name:</label>
+     <!--<input [(ngModel)] = 'hero.name' placeholder=' name'/>-->
+     {{name}}
+    </div>
+    <h3>My Heroes</h3>
+    <ul class="heroes">
+        <li *ngFor="let hero of heroes" (click)="getInfo(hero)">
+            <span class="badge">{{hero.id}}</span>{{hero.name}}
+        </li>
+    </ul>
+    
+     <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hero tour!';
+  name = 'Heroes';
+  id = '1';
+
+  heroes: Hero[] = [
+    {name:'Rocky',id:2, age:27},
+    {name:'Umal',id:3, age:34},
+    {name:'Samiul',id:4, age:22},
+  ];
+
+  selectedHero: Hero;
+
+  getInfo(hero: Hero){
+    this.selectedHero = hero;
+  }
 }
